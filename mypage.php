0962940b8b75f45d5ebc8e32f5dc76d177a764d8
nyan <apple@gmail.com>
2019-05-14 15:10:57 +0900

    投稿機能実装

--- mypage.php
+++ mypage.php
@@ -1,19 +1,50 @@
 <?php
 require_once('config.php');
 
+//アクセス制限
 if (!isset($_SESSION['user_id'])){
   $_SESSION['flash'] = "ログインしてください";
   header('Location:login_form.php');
 }
 
 try {
-  $sql = "select * from users where id = :id";
+  $sql = "select * from users
+          where id = :id";
   $stmt = $pdo->prepare($sql);
   $stmt->execute(array(':id' => $_SESSION['user_id']));
   $user = $stmt->fetch(PDO::FETCH_ASSOC);
 } catch (\Exception $e) {
   echo $e->getMessage() . PHP_EOL;
-  $error_messages[] = 'error';
+}
+
+try{
+  $sql = "select post_content
+          from users inner join posts on users.id = posts.user_id
+          where :id = posts.user_id
+          order by posts.created_at desc";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute(array(':id' => $user['id']));
+  $user_posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (\Exception $e) {
+  echo $e->getMessage() . PHP_EOL;
+}
+
+if(!empty($_POST)){
+
+  $post_content = $_POST['content'];
+  $user_id = $user['id'];
+
+  try {
+    $sql = "insert into posts(user_id,post_content,created_at) value(:user_id,:post_content,:created_at)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(':user_id' => $user_id , ':post_content' => $post_content , ':created_at' => date('Y-m-d H:i:s')));
+
+    $_SESSION['flash'] = '投稿しました';
+    header('Location:mypage.php');
+  } catch (\Exception $e) {
+    echo $e->getMessage() ;
+    $_SESSION['flash'] = 'error';
+  }
 }
 
  ?>
@@ -38,15 +69,20 @@ try {
           ようこそ
           <?php  echo $user['name']; ?>
           さん
+          <p>id = <?php echo $user['id'] ?></p>
         </div>
         <div class="mypage_right">
           <form class ="post_form" action="#" method="post">
-            <textarea name="name" rows="8" cols="80"></textarea><br>
+            <textarea name="content" rows="8" cols="80"></textarea><br>
             <input id="post_btn" type="submit" name="" value="投稿">
           </form>
-          <?php foreach($user as $info): ?>
-          <p><?php echo $info ?></p>
+
+          <?php foreach($user_posts as $post): ?>
+            <?php foreach ($post as $key => $value): ?>
+              <p><?php echo $value ?></p>
+            <?php endforeach; ?>
           <?php endforeach ?>
+
         </div>
       </div>
     </div>
