cc4cf4ebbd55cf5a903d0dadd8e6e79df3f0664e
nyan <apple@gmail.com>
2019-05-23 16:06:16 +0900

    ヘッダーメニューボタン実装

--- header.php
+++ header.php
@@ -12,8 +12,16 @@
           <li><a href="signup_form.php">ユーザー登録</a></li>
           <li><a href="login_form.php">ログイン</a></li>
         <?php else:?>
+          <li id="toggle_menu" ><a>メニュー</a>
+            <ul id="child" class="close">
+              <li><a href="prof_edit.php">プロフィール編集</a></li>
+              <li><a href="logout_process.php">ログアウト</a></li>
+              <li>test</li>
+              <li>test</li>
+            </ul>
+          </li>
           <li><a href="mypage.php?page_id=<?php echo $_SESSION['user_id'] ?>">マイページ</a></li>
-          <li><a href="logout_process.php">ログアウト</a></li>
+
         <?php endif ?>
       </ul>
     </nav>
--- js/common.js
+++ js/common.js
@@ -1,5 +1,10 @@
 // メッセージ表示
 if($('#js_show_msg').text().replace(/^[\s　]+|[\s　]+$/g, "").length){
   $('#js_show_msg').slideToggle('slow');
-  setTimeout(function(){ $('#js_show_msg').slideToggle('slow'); }, 3000);
+  setTimeout(function(){ $('#js_show_msg').slideToggle('slow'); }, 2000);
   }
+
+//ヘッダーメニュー
+$('#toggle_menu').on('click',function(){
+  $('#child').toggleClass('close');
+});
