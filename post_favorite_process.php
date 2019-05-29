44c112c22228614afdda7477a4d3fb5ab53f44e0
nyan <apple@gmail.com>
2019-05-29 20:03:29 +0900

    お気に入り処理の重複部分をまとめた

--- post_favorite_process.php
+++ post_favorite_process.php
@@ -3,54 +3,36 @@ debug('お気に入り追加のPOST送信があります');
 $post_id = $_POST['post_id'];
 $prev_page = basename($_SERVER['HTTP_REFERER']);
 
-//既に登録されていたら削除
+//既に登録されているか確認
 if(check_favolite_duplicate($current_user['id'],$post_id)){
-  try{
-    $dbh = dbConnect();
-    $sql = "DELETE
-            FROM favorite
-            WHERE :user_id = user_id AND :post_id = post_id";
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute(array(':user_id' => $current_user['id'] , ':post_id' => $post_id));
-
-    if(query_result($stmt)){
-      debug('お気に入り解除成功');
-      set_flash('error','お気に入り解除しました');
-
-      header("Location:$prev_page");
-      exit();
-    }
-  } catch (\Exception $e) {
-    error_log('エラー発生:' . $e->getMessage());
-    set_flash('error',ERR_MSG1);
-  }
-  debug('お気に入り解除失敗');
-
-  header("Location:$prev_page");
-  exit();
-
-//登録されていなければ登録
+  $action = '解除';
+  $flash_type = 'error';
+  $sql = "DELETE
+          FROM favorite
+          WHERE :user_id = user_id AND :post_id = post_id";
 }else{
-  try{
-    $dbh = dbConnect();
-    $sql = "INSERT INTO favorite(user_id,post_id)
-            VALUES(:user_id,:post_id)";
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute(array(':user_id' => $current_user['id'] , ':post_id' => $post_id));
-
-    if(query_result($stmt)){
-      debug('お気に入り登録成功');
-      set_flash('sucsess','お気に入りに登録しました');
-
-      header("Location:$prev_page");
-      exit();
-    }
-  } catch (\Exception $e) {
-    error_log('エラー発生:' . $e->getMessage());
-    set_flash('error',ERR_MSG1);
-  }
-  debug('お気に入り登録失敗');
-
-  header("Location:$prev_page");
-  exit();
+  $action = '登録';
+  $flash_type = 'sucsess';
+  $sql = "INSERT INTO favorite(user_id,post_id)
+          VALUES(:user_id,:post_id)";
 }
+try{
+  $dbh = dbConnect();
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute(array(':user_id' => $current_user['id'] , ':post_id' => $post_id));
+
+  if(query_result($stmt)){
+    debug("お気に入り${action}成功");
+    set_flash("${flash_type}","お気に入り${action}しました");
+
+    header("Location:$prev_page");
+    exit();
+  }
+} catch (\Exception $e) {
+  error_log('エラー発生:' . $e->getMessage());
+  set_flash('error',ERR_MSG1);
+}
+debug("お気に入り${action}失敗");
+
+header("Location:$prev_page");
+exit();
